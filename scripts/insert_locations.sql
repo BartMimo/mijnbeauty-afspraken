1973f12aa29f98073dedd0837ecc3fd196ad04f0
BartvGem <bart.vangemert@gemeentemaashorst.nl>
2026-01-16 15:21:32 +0100

    feat: add alternative INSERT script for locations
    - Added insert_locations.sql with sample inserts
    - Provides simple alternative to COPY for testing

---
+++ scripts/insert_locations.sql
@@ -0,0 +1,13 @@
+-- Alternative: Direct SQL insert for locations
+-- If COPY doesn't work, you can run this instead
+-- This inserts locations one by one (may take longer but avoids file upload)
+INSERT INTO locations (city, postcode, latitude, longitude, province) VALUES ('Amsterdam', '1000', 52.3362429, 4.8694444, 'Noord-Holland');
+INSERT INTO locations (city, postcode, latitude, longitude, province) VALUES ('Amsterdam', '1001', 52.36424, 4.883358, 'Noord-Holland');
+INSERT INTO locations (city, postcode, latitude, longitude, province) VALUES ('Amsterdam', '1002', 52.36424, 4.883358, 'Noord-Holland');
+INSERT INTO locations (city, postcode, latitude, longitude, province) VALUES ('Amsterdam', '1003', 52.36424, 4.883358, 'Noord-Holland');
+INSERT INTO locations (city, postcode, latitude, longitude, province) VALUES ('Amsterdam', '1005', 52.36424, 4.883358, 'Noord-Holland');
+INSERT INTO locations (city, postcode, latitude, longitude, province) VALUES ('Amsterdam', '1006', 52.36424, 4.883358, 'Noord-Holland');
+INSERT INTO locations (city, postcode, latitude, longitude, province) VALUES ('Amsterdam', '1007', 52.36424, 4.883358, 'Noord-Holland');
+INSERT INTO locations (city, postcode, latitude, longitude, province) VALUES ('Amsterdam', '1008', 52.36424, 4.883358, 'Noord-Holland');
+INSERT INTO locations (city, postcode, latitude, longitude, province) VALUES ('Amsterdam', '1009', 52.36424, 4.883358, 'Noord-Holland');
+INSERT INTO locations (city, postcode, latitude, longitude, province) VALUES ('Amsterdam', '1011', 52.3729762, 4.9039565, 'Noord-Holland');
